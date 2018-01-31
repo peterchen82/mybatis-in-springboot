027fc132ebefda985832abe6e89dadfc71aa0557
陈杰 <chenjie@wejing.cn>
2018-01-31 15:50:58 +0800

    增加阿里代码规约的pmd支持

---
+++ src/main/java/com/github/peterchen82/mybatis/entity/Artcle.java
@@ -0,0 +1,58 @@
+package com.github.peterchen82.mybatis.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import com.alibaba.fastjson.JSON;
+
+
+/**
+ * 文章实体对象
+ * @author peterchen
+ */
+public class Artcle implements Serializable {
+
+    private static final long serialVersionUID = 2629983876059197650L;
+
+    private Long id;
+    private Date created;
+    private String title;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+    @Override
+    public String toString(){
+        return JSON.toJSONString(this);
+    }
+}
